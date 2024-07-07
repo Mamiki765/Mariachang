5c4de08ffd0169674d1ac718cadb46b6239a53e7
Glitch (mariachang) <none>
2024-07-07 19:39:35 +0000

    😬🎾 Checkpoint ./commands/slashs/gacha.mjs:68612097/310

--- commands/slashs/gacha.mjs
+++ commands/slashs/gacha.mjs
@@ -2,7 +2,7 @@ import { SlashCommandBuilder } from "discord.js";
 
 export const data = new SlashCommandBuilder()
   .setName("gacha")
-  .setDescription("闇市を引くにゃ！");
+  .setDescription("闇市を引くにゃ！(11連対応！)");
 
 export async function execute(interaction) {
   const arr = ["レリック　虹のにぼし", "アーティファクト　金のにぼし", "ハイクオリティ　銀のにぼし", "レギュラー　銅のにぼし", "ノービス　にぼし", "カースド　にぼし（賞味期限切れ）"];
@@ -13,16 +13,19 @@ export async function execute(interaction) {
   for (let i = 0; i < weight.length; i++) {
     totalWeight += weight[i];
   }
-  let random = Math.floor(Math.random() * totalWeight);
-  
-  for (let i = 0; i < weight.length; i++) {
-    if (random < weight[i]) {
-      result = arr[i];
-      break;
-    } else {
-      random -= weight[i];
+  for (let j = 1; j < 12; j++) {
+    let random = Math.floor(Math.random() * totalWeight);
+    for (let i = 0; i < weight.length; i++) {
+      if (random < weight[i]) {
+        result += arr[i] + "\n";
+        break;
+      } else {
+        random -= weight[i];
+      }
     }
-  }
+  }  
 
-  await interaction.reply(`${result} が排出されました！`);
+  await interaction.reply({
+    flags: [ 4096 ],
+    content: `結果\n${result} `});
 }
