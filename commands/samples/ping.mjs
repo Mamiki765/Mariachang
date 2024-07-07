8af6f3cecd0c4c50e83d79b8e4eb624e6124d98c
Glitch (mariachang) <none>
2024-07-07 12:25:26 +0000

    Rewound to commit 6a14e5b7e07f32a328c1f4940013da0ac534269c + 0 edits

--- commands/samples/ping.mjs
+++ commands/samples/ping.mjs
@@ -2,7 +2,7 @@ import { SlashCommandBuilder } from 'discord.js';
 
 export const data = new SlashCommandBuilder()
   .setName('ping')
-  .setDescription('botが生きてるかチェックできるにゃ');
+  .setDescription('サーバーが生きてるかチェックできるにゃ');
 
 export async function execute(interaction){
 	await interaction.reply('ぽんにゃ！');
