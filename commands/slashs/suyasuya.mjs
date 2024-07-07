8c32334729059c0bf15f3ff247b964349da5fb5d
Glitch (mariachang) <none>
2024-07-07 14:45:12 +0000

    🕷🏫 Checkpoint ./commands/slashs/suyasuya.mjs:68612097/2350

--- commands/slashs/suyasuya.mjs
+++ commands/slashs/suyasuya.mjs
@@ -1,46 +1,17 @@
-import { SlashCommandBuilder } from 'discord.js';
-import { Client, Intents, MessageActionRow, MessageButton } from 'discord.js';
+import {
+  SlashCommandBuilder
+} from "discord.js";
+
 
 export const data = new SlashCommandBuilder()
   .setName('suyasuya')
-  .setDescription('（未実装）【！注意！】発言ができなくなります！（自粛用にどうぞ）')
+  .setDescription('【！注意！】発言ができなくなります！（自粛用にどうぞ）')
   .addStringOption(option =>
     option
       .setName('minutes')
       .setDescription('タイムアウトする時間を分単位で入力してください（１-720）')
       .setRequired(true)
   );
-
-// 解除ボタン
-class TimeoutView {
-    constructor(member) {
-        this.member = member;
-        this.view = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                    .setCustomId('untimeout_button')
-                    .setLabel('→起こす←')
-                    .setStyle('DANGER')
-            );
-    }
-
-    // 起こすボタンのハンドラ
-    async handleButton(interaction) {
-        await this.member.timeout(null); // メソッド名は適宜変更する必要があります
-        await interaction.message.delete();  // メッセージを削除
-
-        await interaction.reply({
-            content: `${this.member.toString()}のタイムアウトが解除されました。`,
-            ephemeral: true,
-            fetchReply: false
-        });
-    }
-
-    // ビューの行を取得するメソッド
-    getView() {
-        return this.view;
-    }
-}
 
 export async function execute(interaction){
   const nerunonya = interaction.options.getString('minutes');
@@ -56,10 +27,11 @@ export async function execute(interaction){
             ephemeral: true
         });
     }
-	await interaction.reply({
-    content: '${nerunonya}分封殺してやるにゃ！精々作業なり睡眠なりするにゃ！',
-    components: [timeoutView.getView()],
-    fetchReply: false
-  });
   await interaction.member.timeout(60 * 1000 * nerunonya, "/suyasuyaによるセルフタイムアウト");
+  let suyasuya2 = "";
+  suyasuya2 = nerunonya;
+	await interaction.reply({
+    content: "${suyasuya2}" + '分封殺してやるにゃ！精々作業なり睡眠なりするにゃ！'
+  });
+
 }
