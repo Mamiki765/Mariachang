fc000a852e7ff14e4488615bdb1ef22c278f3889
Glitch (mariachang) <none>
2024-07-07 16:26:14 +0000

    🛩❄️ Checkpoint ./handlers/messageCreate.mjs:68612097/701

--- handlers/messageCreate.mjs
+++ handlers/messageCreate.mjs
@@ -9,15 +9,16 @@ export default async(message) => {
     await message.reply("https://cdn.discordapp.com/attachments/1025416223724404766/1110586574598570094/image.png?ex=668c0259&is=668ab0d9&hm=fa9f6ffa7cb5a3641a7334129abe2a2a1af83e90bb6a06a983cf27e6bbca3bf4&");
   }
 
-    if (message.content.match(/r2p\d{6}/)) {
+    if (message.content.match(/^r2[pn][0-9]/)) {
     await message.reply("https://rev2.reversion.jp/character/detail/" + message.content);
   }
   
 //　　if (message.content === "\?にゃん" || "\?にゃーん" || "\?にゃ～ん"){   
-//    await message.reply("にゃ～ん");
-//  }
+  if (message.content.match(/^(!にゃん|!にゃーん|!にゃ～ん|!にゃあん)$/)) {
+    await message.reply("にゃ～ん");
+  }
 
-  if (message.content.match(/タイムアウトして/)) {
+  if (message.content.match(/^タイムアウトして$/)) {
     await message.reply("６０秒封殺にゃ");
     await message.member.timeout(60 * 1000, "メッセージを送ってきたから");
   }
