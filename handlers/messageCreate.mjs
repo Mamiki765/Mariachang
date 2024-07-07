30f7e8e1074d9b93a541184c3e4601ab4dd27cda
Glitch (mariachang) <none>
2024-07-07 15:52:55 +0000

    🌮🌆 Checkpoint ./handlers/messageCreate.mjs:68612097/653

--- handlers/messageCreate.mjs
+++ handlers/messageCreate.mjs
@@ -8,8 +8,12 @@ export default async(message) => {
   if (message.content.match(/^ニョワミヤ|ﾆｮﾜﾐﾔ|ニョワミヤリカ|ﾆｮﾜﾐﾔﾘｶ$/)) {
     await message.reply("https://cdn.discordapp.com/attachments/1025416223724404766/1110586574598570094/image.png?ex=668c0259&is=668ab0d9&hm=fa9f6ffa7cb5a3641a7334129abe2a2a1af83e90bb6a06a983cf27e6bbca3bf4&");
   }
+
+    if (message.content.match(/r2p\d{6}/)) {
+    await message.reply("https://rev2.reversion.jp/character/detail/" + message.content);
+  }
   
-  if (message.content.match(/^!（にゃん|にゃーん|にゃ～ん）$/)) {
+  if (message.content.match(/^\!（にゃん|にゃーん|にゃ～ん）$/)) {
     await message.reply("にゃ～ん");
   }
 
