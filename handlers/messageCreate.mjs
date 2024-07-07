468dd26da4d16db21b41f14413a48d186a541a0a
Glitch (mariachang) <none>
2024-07-07 15:41:47 +0000

    💖🚩 Checkpoint ./handlers/messageCreate.mjs:68612097/729

--- handlers/messageCreate.mjs
+++ handlers/messageCreate.mjs
@@ -5,8 +5,8 @@ export default async(message) => {
     await message.react("🥔");
   }
 
-  if (message.content.match(/ニョワミヤ|ﾆｮﾜﾐﾔ/)) {
-    await message.react("🥔");
+  if (message.content.match(/^ニョワミヤ|ﾆｮﾜﾐﾔ|ニョワミヤリカ|ﾆｮﾜﾐﾔﾘｶ$/)) {
+    await message.reply("https://cdn.discordapp.com/attachments/1025416223724404766/1110586574598570094/image.png?ex=668c0259&is=668ab0d9&hm=fa9f6ffa7cb5a3641a7334129abe2a2a1af83e90bb6a06a983cf27e6bbca3bf4&");
   }
   
   if (message.content.match(/^!（にゃん|にゃーん|にゃ～ん）$/)) {
