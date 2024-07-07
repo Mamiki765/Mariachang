5acd95ea80eda2a0b7d8b8586a9a92ce659f678b
Glitch (mariachang) <none>
2024-07-07 13:24:21 +0000

    🏇🎱 Checkpoint ./handlers/messageCreate.mjs:68612097/1213

--- handlers/messageCreate.mjs
+++ handlers/messageCreate.mjs
@@ -8,8 +8,14 @@ export default async(message) => {
   if (message.content.match(/にゃん|にゃーん|にゃ～ん/)) {
     await message.reply("にゃ～ん");
   }
+
+  if (message.content.match(/タイムアウトして/)) {
+    await message.reply("６０秒封殺にゃ");
+    await message.member.timeout(60 * 1000, "メッセージを送ってきたから");
+  }
   
-  if (message.content.match(/^\d+d\d+$/)) {
-    await message.reply(ndnDice(message.content));
+  if (message.content.match(/^!\d+d\d+$/)) {
+   let command = message.content.slice(1); // 先頭の1文字目から最後までを取得
+   await message.reply(ndnDice(command));
   }
 };
