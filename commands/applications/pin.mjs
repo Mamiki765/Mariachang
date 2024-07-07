e0c7e6c5fc9bf31b8d95b3b2dcd1c19c485626af
Glitch (mariachang) <none>
2024-07-07 20:37:36 +0000

    🌓🌌 Checkpoint ./commands/applications/pin.mjs:68612097/49

--- commands/applications/pin.mjs
+++
@@ -1,5 +0,0 @@
-const { ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.js');
-
-const data = new ContextMenuCommandBuilder()
-	.setName('ピン留め（未実装）')
-	.setType(ApplicationCommandType.Message);
