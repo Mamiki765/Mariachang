79a7daa1fb7ebf725100d81cacafc345adbe162f
Glitch (mariachang) <none>
2024-07-14 08:32:16 +0000

    🤗🐑 Checkpoint ./commands/contexts/pindome.mjs:68612097/76

--- commands/contexts/pindome.mjs
+++ commands/contexts/pindome.mjs
@@ -26,10 +26,10 @@ export async function execute(interaction) {
     		});
     		if (message.pinned){
     			await message.unpin();
-    			interaction.reply("メッセージのピン留めを解除しました。");
+    			interaction.reply("メッセージのピン留めを解除しました。\n対象：" + message.url);
     		} else {
     			await message.pin();
-    			interaction.reply("メッセージのピン留めをしました。");
+    			interaction.reply("メッセージのピン留めをしました。\n対象：" + message.url);
     		};
  	};
  
