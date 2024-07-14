fa7ef2b2cdaffb8eec8c600b3eab63406aebeee7
Glitch (mariachang) <none>
2024-07-14 04:19:00 +0000

    💾💏 Checkpoint ./handlers/messageCreate.mjs:68612097/173

--- handlers/messageCreate.mjs
+++ handlers/messageCreate.mjs
@@ -112,8 +112,10 @@ export default async(message) => {
 
   if (message.content.match(/^(今何時？|いまなんじ？|今日何日？|きょう何日？)$/)) {
     const date = new Date();
-    const nanjidate = date.getFullYear() +"年" + date.getMonth()+1 +"月" +date.getDate() + "日" + date.getHours() +"時"+date.getMinutes() +"分"+date.getSeconds()+"秒";
-    await message.reply(`${nanjidate}ですにゃ。`);
+    const nanjimonth = date.getMonth()+1;
+    const masiroyear = date.getFullYear()+28;
+    const nanjidate = date.getFullYear() +"年" + nanjimonth +"月" +date.getDate() + "日" + date.getHours() +"時"+date.getMinutes() +"分"+date.getSeconds()+"秒";
+    await message.reply(`${nanjidate}ですにゃ。\nマシロ市は${masiroyear}年ですにゃ。`);
 //    await message.member.timeout(60 * 1000, "「タイムアウトして」により");
   }
 
