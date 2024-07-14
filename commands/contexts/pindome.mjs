6e78fcf0329581736027cd654014350d92b79523
Glitch (mariachang) <none>
2024-07-14 08:20:37 +0000

    🛣🌼 Checkpoint ./commands/contexts/pindome.mjs:68612097/691

--- commands/contexts/pindome.mjs
+++ commands/contexts/pindome.mjs
@@ -1,12 +1,14 @@
 import { ContextMenuCommandBuilder,  ApplicationCommandType, EmbedBuilder , PermissionsBitField} from "discord.js";
 
 export const data = new ContextMenuCommandBuilder()
-  .setName("テスト")
+  .setName("ピン留めの登録/解除")
   .setType(ApplicationCommandType.Message);
 
 export async function execute(interaction) {
+    if (!interaction.guild) return;
  		const { channel } = interaction;
- 		if (!channel.permissionsFor(PermissionsBitField.Flags.ViewChannel))
+ 	/*管理権限あげてるので見つかるわけがない
+  if (!channel.permissionsFor(PermissionsBitField.Flags.ViewChannel))
  			return interaction.reply({
  				content: "BOTにチャンネル閲覧の権限がありません。",
  				ephemeral: true
@@ -16,6 +18,7 @@ export async function execute(interaction) {
     				content: "BOTにメッセージ管理の権限がありません。",
     				ephemeral: true
     			});
+          */
     		const message = interaction.options.getMessage("message")
     		if (message.system) return interaction.reply({
     			content: "システムメッセージはピン留めができません。",
