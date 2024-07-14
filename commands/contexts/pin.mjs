c0494956f730fbf24a8ef5900b0aa5ecc918177d
Glitch (mariachang) <none>
2024-07-14 07:33:54 +0000

    🤘🐡 Checkpoint ./commands/contexts/pin.mjs:68612097/1356

--- commands/contexts/pin.mjs
+++ commands/contexts/pin.mjs
@@ -1,5 +1,34 @@
-import { ContextMenuCommandBuilder,  ApplicationCommandType, EmbedBuilder} from "discord.js";
+import { ContextMenuCommandBuilder,  ApplicationCommandType, EmbedBuilder , PermissionsBitField} from "discord.js";
 
 export const data = new ContextMenuCommandBuilder()
-  .setName("何も起きない")
+  .setName("pin")
   .setType(ApplicationCommandType.Message);
+
+export async function execute(interaction) {
+ 	if (interaction.isMessageContextMenuCommand()){
+ 		if (!interaction.guild) return;
+ 		const { channel } = interaction;
+ 		if (!channel.permissionsFor(PermissionsBitField.Flags.ViewChannel))
+ 			return interaction.reply({
+ 				content: "BOTにチャンネル閲覧の権限がありません。",
+ 				ephemeral: true
+ 			});
+ 		if (!channel.permissionsFor(PermissionsBitField.Flags.ManageMessages))
+    			return interaction.reply({
+    				content: "BOTにメッセージ管理の権限がありません。",
+    				ephemeral: true
+    			});
+    		const message = interaction.options.getMessage("message")
+    		if (message.system) return interaction.reply({
+    			content: "システムメッセージはピン留めができません。",
+    			ephemeral: true
+    		});
+    		if (message.pinned){
+    			await message.unpin();
+    			interaction.reply("メッセージのピン留めを解除しました。");
+    		} else {
+    			await message.pin();
+    			interaction.reply("メッセージのピン留めをしました。");
+    		};
+ 	};
+ }
