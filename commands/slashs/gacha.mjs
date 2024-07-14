f136033787cfccee8dca404dcb49a57c7412144e
Glitch (mariachang) <none>
2024-07-14 06:01:20 +0000

    🚂🚤 Checkpoint ./commands/slashs/gacha.mjs:68612097/602

--- commands/slashs/gacha.mjs
+++ commands/slashs/gacha.mjs
@@ -27,13 +27,14 @@ export async function execute(interaction) {
   }  
   
     const embed = new EmbedBuilder()
-      .setColor(0x5cb85c)
-      .setAuthor({ name: newState.member.displayName, iconURL: newState.member.displayAvatarURL()})
-      .setTitle(`<#${newState.channel.id}> で通話を開始しました！`)
-      .setTimestamp();
+      .setColor(0xEFDCAA)
+      .setAuthor({ name: interaction.member.displayName, iconURL: interaction.member.displayAvatarURL()})
+      .setTitle(`にぼしガチャ11連を回しました`)
+      .setDescription(result)
+      .setFooter({text: "（PPP闇市仕様）"})
   
   await interaction.reply({
     flags: [ 4096 ],//silent
     embeds: [embed]
-    content: `結果\n${result} `});
+    });
 }
