f58f9c7d7be4066f7d9d55cc0db8a23b0f604fbc
Glitch (mariachang) <none>
2024-07-15 05:30:15 +0000

    🗾🏇 Checkpoint ./maps/help.js:68612097/24 ./commands/slashs/help.mjs:68612097/325 ./maps/help.txt:68612097/123

--- commands/slashs/help.mjs
+++ commands/slashs/help.mjs
@@ -1,18 +1,19 @@
 import { SlashCommandBuilder,  EmbedBuilder } from 'discord.js';
-
+import fs from "fs";
 
 export const data = new SlashCommandBuilder()
   .setName('help')
   .setDescription('このBOTの機能を説明します')
 
 export async function execute(interaction) {
-await interaction.reply({ 
-    flags: [ 4096 ],
-    embeds: [
-                    new EmbedBuilder()
-                    .setTitle("神谷マリアbotについて")
-                    .setDescription("神谷マリアbotは云々カンヌン・・・悪いけど未実装にゃ")
-                    .setColor("#B78CFE")
-                ]
-  });
+  const helptext = fs.readFileSync("../maps/help.txt", 'utf8');
+  await interaction.reply({ 
+      flags: [ 4096 ],
+      embeds: [
+                      new EmbedBuilder()
+                      .setTitle("神谷マリアbotについて")
+                      .setDescription(helptext)
+                      .setColor("#B78CFE")
+                  ]
+    });
 }
