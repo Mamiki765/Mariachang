9e4a08585df5eaac330f35663cb66e19915746f2
Glitch (mariachang) <none>
2024-07-15 05:18:58 +0000

    🏂📊 Checkpoint ./commands/slashs/help.mjs:68612097/1649

--- commands/slashs/help.mjs
+++ commands/slashs/help.mjs
@@ -4,3 +4,15 @@ import { SlashCommandBuilder,  EmbedBuilder } from 'discord.js';
 export const data = new SlashCommandBuilder()
   .setName('help')
   .setDescription('このBOTの機能を説明します')
+
+export async function execute(interaction) {
+await interaction.reply({ 
+    flags: [ 4096 ],
+    embeds: [
+                    new EmbedBuilder()
+                    .setTitle("神谷マリアbotについて")
+                    .setDescription("神谷マリアbotは云々カンヌン・・・悪いけど未実装にゃ")
+                    .setColor("#B78CFE")
+                ]
+  });
+}
