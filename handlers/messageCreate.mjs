4b28e91658b17fa3eac94820b07189f4c5a852f6
Glitch (mariachang) <none>
2024-07-17 22:35:39 +0000

    🍲🎸 Checkpoint ./handlers/messageCreate.mjs:68612097/712

--- handlers/messageCreate.mjs
+++ handlers/messageCreate.mjs
@@ -55,17 +55,23 @@ export default async(message) => {
       content: "https://media.discordapp.net/attachments/1025416223724404766/1122185542252105738/megamoji.gif?ex=668cad7a&is=668b5bfa&hm=5c970ab0422c8731d0471ab1d65663b76ae6fd8fb47192481bdbbdadcd792675&"
   });
   }
+  //https://cdn.discordapp.com/attachments/1261485824378142760/1263261822757109770/IMG_2395.gif?ex=669997c0&is=66984640&hm=a12e30f8b9d71ffc61ab35cfa095a8b7f7a08d04988f7b33f06437b13e6ee324&
   if (message.content.match(/^(ゆゔさや|ゆヴさや|ゆずさや)$/)) {
     await message.reply({
      flags: [ 4096 ],//@silentになる
       content: "https://media.discordapp.net/attachments/1040261246538223637/1175700688219672587/megamoji_4.gif?ex=668ce817&is=668b9697&hm=e26e24e90dc3bd6606255aaefd4f7ad91118f1d8cc5a6be8f48013b7ca2fa58a&"
   });
   } 
+  if (message.content.match(/^(ゆ○さや|ゆ●さや)$/)) {
+    await message.reply({
+     flags: [ 4096 ],//@silentになる
+      content: "attachments/10402612465382
+  });
+  } 
 //画像いたずら系ここまで
   
 
 //ここからステシ変換
-  //ロスアカ
     if (message.content.match(/^r2[pn][0-9][0-9][0-9][0-9][0-9][0-9]$/)) {
     await message.reply({
       flags: [ 4096 ],//@silent
