fa0f7a219134fb8e13e3bf5639859bdfb2326a60
Glitch (mariachang) <none>
2024-07-23 05:30:26 +0000

    🌱🔨 Checkpoint ./handlers/messageCreate.mjs:68612097/3

--- handlers/messageCreate.mjs
+++ handlers/messageCreate.mjs
@@ -21,7 +21,7 @@ export default async(message) => {
     if(Math.floor(Math.random() * 100) < 1){ //0-99 1%で大当たり
       await message.react('1265162645330464898'); await message.react('1265165857445908542');
       await message.react('1265165940824215583'); await message.react('1265166237399388242');
-      await message.react('1265166293464518666'); await message.react("‼");
+      await message.react('1265166293464518666'); await message.react("‼️");
     }else{
       const toruchan =['1264756212994674739','1265162812758687754'
                        ,'1265163072016879636','1265163139637317673'
