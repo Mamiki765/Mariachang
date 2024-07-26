518bf7f9f44b6dcbdfd1440e3ae1ba6884280ec0
Glitch (mariachang) <none>
2024-07-26 15:51:02 +0000

    🏇⏰ Checkpoint ./commands/slashs/laplace.mjs:68612097/861

--- commands/slashs/laplace.mjs
+++ commands/slashs/laplace.mjs
@@ -1,4 +1,5 @@
 import { SlashCommandBuilder ,  EmbedBuilder} from "discord.js";
+import fs from "fs";
 
 export const data = new SlashCommandBuilder()
   .setName("laplace")
@@ -7,6 +8,17 @@ export const data = new SlashCommandBuilder()
 export async function execute(interaction) {
   // C15　N735 R22０　HQ3０　 1000倍して悪い順に
   const arr = ["__C__  ", "N  "　, "R  ","### HQ　"];
+//240727追加　各レアリティごとのファイルを読み込む
+  const map0 = fs.readFileSync("./maps/cursed.txt", 'utf8');
+  const map1 = fs.readFileSync("./maps/novice.txt", 'utf8');
+  const map2 = fs.readFileSync("./maps/regular.txt", 'utf8');
+  const map3 = fs.readFileSync("./maps/HighQuality.txt", 'utf8');
+  const maps = [];
+  maps[0] = map0.split(/\n/);
+  maps[1] = map1.split(/\n/);
+  maps[2] = map2.split(/\n/);
+  maps[3] = map2.split(/\n/);
+//ここまで
   const weight = [15,735,220,30];
   let result = "";
 //確率を足して
@@ -20,7 +32,7 @@ export async function execute(interaction) {
     //result += random + "  ";　　//乱数確認用
     for (let i = 0; i < weight.length; i++) {　
       if (random < weight[i]) {　　//レアリティ決定
-        result += arr[i] + "\n";　//結果を付け足して次に
+        result += arr[i] + maps[i][Math.floor(Math.random() * maps[i].length)] + "\n";　//結果を付け足して次に
         break;
       } else {
         random -= weight[i];
