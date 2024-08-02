123feb694f8fee94d742ef345ef30202c40accee
Glitch (mariachang2) <none>
2024-08-02 02:17:45 +0000

    🐬💧 Checkpoint ./handlers/messageReactionAdd.mjs:68612097/1067

--- handlers/messageReactionAdd.mjs
+++ handlers/messageReactionAdd.mjs
@@ -17,12 +17,25 @@ export default async(reaction, user) => {
       }
     const soudane = reaction.count
     if(reaction.message.reactions.cache.get('1236923430490734672')?.count){return;}
-    if(soudane > 6){
+    if(soudane === 7){
       if(reaction.message.channel.nsfw){
         await reaction.message.reply(`そうだねが7以上に達したため<#1098172139414233108>にコピーされます。`);       
       }else{
         await reaction.message.reply(`そうだねが7以上に達したため<#1098159960942202941>にコピーされます。`);
       }
+      await reaction.message.react('1236923430490734672');
+    }
+  }
+　//hda
+    else if (reaction.emoji.id === '1057293963813453914'){
+    if(!reaction.count){//キャッシュされてなければ取得
+     await reaction.fetch();
+      }
+    if(!reaction.message.channel.nsfw){return;}
+    const hda = reaction.count
+    if(reaction.message.reactions.cache.get('1236923430490734672')?.count){return;}
+    if(hda === 7){
+        await reaction.message.reply(`えっちだが7に達したため<#1098172139414233108>にコピーされます。`);       
       await reaction.message.react('1236923430490734672');
     }
   }
