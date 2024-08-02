26092deea22d4d19c9c0f94c74f7bae995ce8d96
Glitch (mariachang2) <none>
2024-08-02 01:09:35 +0000

    ⛳️🛣 Checkpoint ./handlers/messageReactionAdd.mjs:68612097/638

--- handlers/messageReactionAdd.mjs
+++ handlers/messageReactionAdd.mjs
@@ -1,8 +1,24 @@
 export default async(reaction, user) => {
+  //削除
   if (reaction.emoji.id === '1267692767489036390'){//削除を押されたら
-    console.log(reaction.message);
-    if(reaction.message.content.includes(user.id)){
-      await reaction.message.delete();
+    let message = reaction.message;
+    //まずmessage.contentがあるかみる
+    if(!message.content){
+      message = await reaction.message.fetch();//なければ取得
+    }
+    if(message.content.includes(user.id)){
+      await message.delete();
+    }
+  }
+  //そうだね
+    if (reaction.emoji.id === '1267692767489036390'){//削除を押されたら
+    let message = reaction.message;
+    //まずmessage.contentがあるかみる
+    if(!message.content){
+      message = await reaction.message.fetch();//なければ取得
+    }
+    if(message.content.includes(user.id)){
+      await message.delete();
     }
   }
 };
