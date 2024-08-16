d89cbe5c48a73ae10a2c5a2dd65d874c4a4b7c1c
Glitch (mariachang2) <none>
2024-08-16 23:22:16 +0000

    👴💋 Checkpoint ./commands/slashs/admin.mjs:68612097/2390 ./config.mjs:68612097/168

--- commands/slashs/admin.mjs
+++ commands/slashs/admin.mjs
@@ -1,5 +1,7 @@
 import { SlashCommandBuilder,  EmbedBuilder , PermissionsBitField} from 'discord.js';
 
+import config from "../../config.mjs"
+
 export const data = new SlashCommandBuilder()
   .setName("admin")
   .setDescription("管理用")
@@ -11,7 +13,7 @@ export const data = new SlashCommandBuilder()
       .addStringOption(option =>
     option
       .setName('content')
-      .setDescription('発言内容を記述してください')
+      .setDescription('発言内容を記述(改行は\n、<br>、@@@のどれかでできます)')
       .setRequired(true)
     )
   );
@@ -19,9 +21,30 @@ export const data = new SlashCommandBuilder()
 export async function execute(interaction) {
  const subcommand = interaction.options.getSubcommand();
   if(subcommand == "chatasmaria"){
-    const content = interaction.options.getString('content');
+    let content = interaction.options.getString('content');
+    // 改行文字を置き換え
+    content = content
+      .replace(/@@@/g, '\n')
+      .replace(/<br>/g, '\n')
+      .replace(/\\n/g, '\n');
+    
     await interaction.channel.send({
      content: content 
+    });
+    await interaction.client.channels.cache.get(config.logch.admin).send({
+      content: `管理者メッセージが送信されました。`,
+      flags: [ 4096 ],
+      embeds: [
+                      new EmbedBuilder()
+                      .setTitle("管理者発言ログ")
+                      .setColor("#FFD700")
+                      .setDescription(`送信内容\`\`\`\n${content}\n\`\`\``)
+                      .addFields(
+                      {
+                        name: "送信チャンネル",
+                        value: `${interaction.channel}`
+                      })
+                  ]
     });
     await interaction.reply({ 
       ephemeral: true,
