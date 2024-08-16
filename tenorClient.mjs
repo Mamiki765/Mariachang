bb049a8c257597776f89843efc73815f6322137c
Glitch (mariachang2) <none>
2024-08-16 15:56:38 +0000

    🐇🌉 Checkpoint ./tenorClient.mjs:68612097/929

--- tenorClient.mjs
+++ tenorClient.mjs
@@ -1,9 +1,7 @@
-//import TenorFetch from 'tenor-fetch';
-
-// TenorのAPIキーを設定
-//const tenorClient = new TenorFetch(process.env.tenor_apikey);
 import TenorFetch from 'tenor-fetch';
 
+//const tenorClient = new TenorFetch(process.env.tenor_apikey);
+try{
 const tf = new TenorFetch(process.env.tenor_apikey);
 
-export default tf;
+export default null;
