2fb69a26a6a9b0f0f67127e5605e32eff37e45fc
Glitch (mariachang2) <none>
2024-08-16 22:58:46 +0000

    👲🕍 Checkpoint ./commands/slashs/admin.mjs:68612097/1976

---
+++ commands/slashs/admin.mjs
@@ -0,0 +1,24 @@
+import { SlashCommandBuilder,  EmbedBuilder , PermissionsBitField} from 'discord.js';
+
+export const data = new SlashCommandBuilder()
+  .setName("admin")
+  .setDescription("管理用")
+// 管理者権限のみで実行可能
+  .setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator.toString())
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName("chatasmaria").setDescription("匿名としてマリアが発言します（作成中）")
+      .addStringOption(option =>
+    option
+      .setName('content')
+      .setDescription('発言内容を記述してください')
+      .setRequired(true)
+    )
+  );
+
+export async function execute(interaction) {
+ const subcommand = interaction.options.getSubcommand();
+  if(subcommand == "chatasmaria"){
+    
+  }
+}
