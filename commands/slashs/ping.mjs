e4a00d4e2493e237ebd0ec4ac054c66247ce66ac
Glitch (mariachang2) <none>
2024-08-16 12:30:02 +0000

    😬👻 Checkpoint ./commands/slashs/ping.mjs:68612097/2876

--- commands/slashs/ping.mjs
+++ commands/slashs/ping.mjs
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder,  EmbedBuilder , ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js';
-const os = require('os');
+import os from 'os';
 import { deletebuttonanyone } from "../../components/buttons.mjs"
 
 export const data = new SlashCommandBuilder()
@@ -9,13 +9,7 @@ export const data = new SlashCommandBuilder()
 export async function execute(interaction){
 //ping確認
   const apiPing = Date.now() - interaction.createdTimestamp
-//メモリ、CPU状態の読み込み
-  const memoryUsage = process.memoryUsage();
-  const cpuUsage = os.cpus().map(cpu => ({
-      model: cpu.model,
-      speed: cpu.speed,
-      times: cpu.times
-  }));
+  
 	await interaction.reply({ 
     flags: [ 4096 ],
 //    content: `<@${interaction.user.id}>`,
@@ -32,25 +26,7 @@ export async function execute(interaction){
                             name: ":yarn:API Endpoint Ping",
                             value: "`" + apiPing + "ms`"
                         },
-                      {
-                    name: 'Memory Usage',
-                    value: `RSS: ${Math.round(memoryUsage.rss / 1024 / 1024)} MB\n` +
-                           `Heap Total: ${Math.round(memoryUsage.heapTotal / 1024 / 1024)} MB\n` +
-                           `Heap Used: ${Math.round(memoryUsage.heapUsed / 1024 / 1024)} MB\n` +
-                           `External: ${Math.round(memoryUsage.external / 1024 / 1024)} MB`,
-                    inline: false
-                },
-                {
-                    name: 'CPU Usage',
-                    value: cpuUsage.map(cpu => 
-                        `Model: ${cpu.model}\n` +
-                        `Speed: ${cpu.speed} MHz\n` +
-                        `User Time: ${cpu.times.user} ms\n` +
-                        `System Time: ${cpu.times.sys} ms\n` +
-                        `Idle Time: ${cpu.times.idle} ms`
-                    ).join('\n\n'),
-                    inline: false
-                }
+                      
                     )
                     .setColor("#2f3136")
                     .setTimestamp()
