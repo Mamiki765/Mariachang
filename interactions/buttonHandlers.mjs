6d517072da7ab5f13f54305fae2447f897d44cba
Glitch (kamiya-maria) <none>
2024-10-18 04:51:30 +0000

    ロスアカのアトリエURLを検知し元画像URLを貼り付ける機能を追加

--- interactions/buttonHandlers.mjs
+++ interactions/buttonHandlers.mjs
@@ -1,62 +1,96 @@
-import {
-  deleteconfirm
-} from "../components/buttons.mjs"
+import { deleteconfirm } from "../components/buttons.mjs";
 import {
   ModalBuilder,
   TextInputBuilder,
   TextInputStyle,
-  ActionRowBuilder
-} from "discord.js"
+  ActionRowBuilder,
+} from "discord.js";
 
 export default async function handleButtonInteraction(interaction) {
+  //以下変数定義
+  //各種IDを含むボタン系の処理
+  //マリアからユーザーにメッセージを送信する送信先
   const DMregex = /^admin_replytoDM-(\d+)$/;
   const DMmatch = interaction.customId.match(DMregex);
+  //本人だけに押せる削除ボタンのチェック
+  const UniqueDeleteregex = /UniqueDelete-(\d+)/;
+  const UniqueDeletematch = interaction.customId.match(UniqueDeleteregex);
+  //以下ボタン処理
   //削除ボタン
-  if (interaction.customId == "delete" || interaction.customId == "deleteanyone") {
+  if (
+    interaction.customId == "delete" ||
+    interaction.customId == "deleteanyone"
+  ) {
     if (!interaction.message.mensions) {
       await interaction.message.fetch();
     } //なければ取得
     //削除ボタンを押されたとき、消せる人がanyoneでないならその本文の文頭にあるメンションの人を投稿者として認識、削除権限の有無を確かめる。
-    const userIdPattern = new RegExp(`^<@${interaction.user.id}>`, 'i'); //自分宛てへのメンションで始まるメッセージなら投稿者
-    if (userIdPattern.test(interaction.message.content) || interaction.customId == "deleteanyone") {
+    const userIdPattern = new RegExp(`^<@${interaction.user.id}>`, "i"); //自分宛てへのメンションで始まるメッセージなら投稿者
+    if (
+      userIdPattern.test(interaction.message.content) ||
+      interaction.customId == "deleteanyone"
+    ) {
       //確認メッセージを送信
       await interaction.reply({
-        content: 'このメッセージを削除しますか？',
+        content: "このメッセージを削除しますか？",
         components: [deleteconfirm],
-        ephemeral: true
+        ephemeral: true,
       });
       return;
-    } else { //削除権限無し
+    } else {
+      //削除権限無し
       await interaction.reply({
-        content: 'このメッセージを削除できるのは投稿者のみです。',
-        ephemeral: true
+        content: "このメッセージを削除できるのは投稿者のみです。",
+        ephemeral: true,
       });
       return;
     }
-  } else if (interaction.customId === 'confirm_delete') {
+  } else if (UniqueDeletematch) {
+    //useridが削除ボタンに入ってるやつ 　UniqueDelete-(ID)
+    //削除ボタンを押されたとき、消せる人がanyoneでないならその本文の文頭にあるメンションの人を投稿者として認識、削除権限の有無を確かめる。
+    const userIdFromCustomId = UniqueDeletematch[1]; // カスタムIDから数字（USERID）を取得
+    if (userIdFromCustomId === interaction.user.id) {
+      //確認メッセージを送信
+      await interaction.reply({
+        content: "このメッセージを削除しますか？",
+        components: [deleteconfirm],
+        ephemeral: true,
+      });
+      return;
+    } else {
+      //削除権限無し
+      await interaction.reply({
+        content: "このメッセージを削除できるのは投稿者のみです。",
+        ephemeral: true,
+      });
+      return;
+    }
+  } else if (interaction.customId === "confirm_delete") {
     // メッセージを削除する処理
-    const messageToDelete = await interaction.channel.messages.fetch(interaction.message.reference.messageId); // 削除するメッセージを取得
+    const messageToDelete = await interaction.channel.messages.fetch(
+      interaction.message.reference.messageId
+    ); // 削除するメッセージを取得
     if (messageToDelete) {
       await messageToDelete.delete();
       await interaction.update({
-        content: 'メッセージが削除されました。',
-        components: []
+        content: "メッセージが削除されました。",
+        components: [],
       });
     } else {
       await interaction.update({
-        content: 'メッセージが見つかりませんでした。',
-        components: []
+        content: "メッセージが見つかりませんでした。",
+        components: [],
       });
     }
     return;
-  } else if (interaction.customId === 'cancel_delete') {
+  } else if (interaction.customId === "cancel_delete") {
     await interaction.update({
-      content: '削除がキャンセルされました。',
-      components: []
+      content: "削除がキャンセルされました。",
+      components: [],
     });
     return;
     //admin系、DMからの返信を受け取るmodal
-  } else if (interaction.customId === 'admin_replyfromDM') {
+  } else if (interaction.customId === "admin_replyfromDM") {
     const modal = new ModalBuilder()
       .setTitle("管理人室に返信します")
       .setCustomId("admin_replyfromDM_submit");
@@ -86,15 +120,16 @@ export default async function handleButtonInteraction(interaction) {
       .setLabel("返信を許可するか(0で禁止)")
       .setCustomId("replyable")
       .setMaxLength(1)
-      .setValue('1')
+      .setValue("1")
       .setRequired(true)
       .setStyle(TextInputStyle.Short);
     const ActionRow = new ActionRowBuilder().setComponents(TextInput);
     const ActionRowSecond = new ActionRowBuilder().setComponents(replyInput);
     modal.setComponents(ActionRow, ActionRowSecond);
     return interaction.showModal(modal);
-  } else { //ボタンが不明のとき
+    //削除ボタンがIDを持っていたときの挙動
+  } else {
+    //ボタンが不明のとき
     return;
   }
-
-};
+}
