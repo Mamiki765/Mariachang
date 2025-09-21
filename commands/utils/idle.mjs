d48ea4294105e15d725a2d1c19c2ba2851531c80
Mamiki765 <179112848+Mamiki765@users.noreply.github.com>
2025-09-21 15:05:44 +0900

    idle.mjs を更新
    文字数制限

--- commands/utils/idle.mjs
+++ commands/utils/idle.mjs
@@ -366,7 +366,7 @@ const pizzaBonusPercentage = Math.log10(idleGame.population) + 1;//仮の数値�
           new ButtonBuilder()
             .setCustomId(`idle_prestige`)
             .setEmoji(config.idle.prestige.emoji)
-            .setLabel(`リセットする(永続 施設Lv+${(pizzaBonusPercentage - 1).toFixed(2)} チップ+${pizzaBonusPercentage.toFixed(3)}%)(未実装)`)
+            .setLabel(`プレステージ(未実装)(Power: ${(pizzaBonusPercentage - 1).toFixed(2)}`)
             .setStyle(ButtonStyle.Danger)
             .setDisabled(true)
         );
